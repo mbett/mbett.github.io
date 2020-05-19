e4e24164062ad062a10773e5b25d2e1a6ca3f59d
mbett <mbett@cs.cmu.edu>
2020-05-18 20:25:08 -0400

    Update index.js

--- flashcards/changehtml/scripts/index.js
+++ flashcards/changehtml/scripts/index.js
@@ -1,1 +1,21 @@
-
+let myFlashcards = [ {
+    "question": "Madame Uppercut\'s age",
+    "answer": 39
+    }, 
+    {
+     "question": "What is the capital of PA?",
+     "answer": "Harrisburg"
+     },
+     {
+     "question": "17*4",
+     "answer": 68
+     }
+     ];
+    
+    
+ let counter = 0;
+ while (counter < myFlashcards.length ) {
+     alert(myFlashcards[counter].question);
+     alert(myFlashcards[counter].answer);
+     ++counter;
+ }
