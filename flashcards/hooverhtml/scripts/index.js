9ccfffea03767b2c4a94c3182e27fb2d5eb89ff5
mbett <mbett@cs.cmu.edu>
2020-05-19 10:40:06 -0400

    Create index.js

---
+++ flashcards/hooverhtml/scripts/index.js
@@ -0,0 +1,35 @@
+let myFlashcards = [ {
+    "question": "Madame Uppercut\'s age",
+    "answer": 39
+    }, 
+    {
+     "question": "What is the capital of PA?",
+     "answer": "Harrisburg"
+     },
+     {
+     "question": "17*4",
+     "answer": 68
+     }
+     ];
+    
+    
+let counter = 0;
+let questionAsked = false; // make sure that they ask the question first!
+
+function displayQuestion() {
+     if (counter == myFlashcards.length)
+         counter = 0;
+     document.getElementById("myQuestion").innerHTML = myFlashcards[counter].question;
+     //document.getElementById("myAnswer").innerHTML = "";
+    questionAsked = true;
+ }
+
+function displayAnswer() {
+     if ( questionAsked == true ) {
+        document.getElementById("myAnswer").innerHTML = myFlashcards[counter].answer;
+        questionAsked = false;
+        ++counter;
+     } else {
+        document.getElementById("myAnswer").innerHTML = "Please click on the question first, then the answer.";
+     }
+ }
