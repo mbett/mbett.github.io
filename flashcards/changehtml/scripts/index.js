e863a8e641648dab97caf0a891148667ad69b02f
mbett <mbett@cs.cmu.edu>
2020-05-19 10:12:51 -0400

    Update index.js

--- flashcards/changehtml/scripts/index.js
+++ flashcards/changehtml/scripts/index.js
@@ -18,7 +18,7 @@ let counter = 0;
 function displayQuestion() {
      if (counter == myFlashcards.length)
          counter = 0;
-     document.getElementById("myQuestion").innerHTML = myFlashcards[counter].question;
+     document.getElementById("myQuestion").innerHTML = myFlashcards[counter].question + counter;
      document.getElementById("myAnswer").innerHTML = "";
  }
 
